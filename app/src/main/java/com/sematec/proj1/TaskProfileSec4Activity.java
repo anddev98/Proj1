2c957a7bf3b5acd31677ae344b174f862d9896af
Developer <you@example.com>
2020-02-08 10:48:55 +0330

    DrawerMenuTask

--- app/src/main/java/com/sematec/proj1/TaskProfileSec4Activity.java
+++ app/src/main/java/com/sematec/proj1/TaskProfileSec4Activity.java
@@ -1,12 +1,15 @@
 package com.sematec.proj1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class TaskProfileSec4Activity extends AppCompatActivity {
 
@@ -22,15 +25,42 @@ public class TaskProfileSec4Activity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = edtName.getText().toString();
-                String family = edtFamily.getText().toString();
-                String email = edtEmail.getText().toString();
-                String phone = edtPhone.getText().toString();
-                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("name",name).apply();
-                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("family",family).apply();
-                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("email",email).apply();
-                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("phone",phone).apply();
-         }
+                AlertDialog dialog = new AlertDialog.Builder(TaskProfileSec4Activity.this)
+                        .setTitle("Attention")
+                        .setMessage("Are you sure save?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String name = edtName.getText().toString();
+                                String family = edtFamily.getText().toString();
+                                String email = edtEmail.getText().toString();
+                                String phone = edtPhone.getText().toString();
+                                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("name",name).apply();
+                                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("family",family).apply();
+                                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("email",email).apply();
+                                PreferenceManager.getDefaultSharedPreferences(TaskProfileSec4Activity.this).edit().putString("phone",phone).apply();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(TaskProfileSec4Activity.this, "cancel save", Toast.LENGTH_SHORT).show();
+
+
+                            }
+                        })
+                        .setNeutralButton("later", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(TaskProfileSec4Activity.this, "Remind me later", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
         });
+
     }
 }
+
